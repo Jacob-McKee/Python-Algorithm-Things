0dd4a4731f26d0af16b365384e9fed205c3ddf7f
Jacob McKee <jacobmckeegames@gmail.com>
2021-02-15 11:38:03 -0600

    Just another easy solution

---
+++ maximum_repeating_substring.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxRepeating(self, sequence: str, word: str) -> int:
+        # So we will incrememt i and multiply it by the word 
+        # and check if each multiplication is in the word
+        i = 1
+
+        while word*i in sequence:
+            i += 1
+
+        return i-1
