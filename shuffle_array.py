c75b387048986f3f64230532054a53e06cc9a8f5
Jacob McKee <jacobmckeegames@gmail.com>
2021-02-17 10:11:51 -0600

    Couple shuffle array solutions

---
+++ shuffle_array.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    # Using list splicing and alternating list adding
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        result = [None] * len(nums)
+        result[::2] = nums[: int(len(nums) / 2)]
+        result[1::2] = nums[int(len(nums) / 2):]
+
+        return result
+
+    def shuffle_with_append(self, nums: List[int], n: int) -> List[int]:
+        l = []
+
+        for i in range(n):
+            l.append(nums[i])
+            l.append(nums[i + n])
+
+        return l
