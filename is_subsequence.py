caeb00753e5552a61f2ff904d4ac61d7542c357f
BuildTools <unconfigured@null.spigotmc.org>
2023-03-03 20:35:57 -0600

    Updated

--- is_subsequence.py
+++ is_subsequence.py
@@ -1,4 +1,5 @@
 class Solution:
+    # First slightly bad way
     def isSubsequence(self, s: str, t: str) -> bool:
         s_index = 0
 
@@ -11,4 +12,19 @@ class Solution:
                         return True
         else:
             return True
+        return False
+    
+    # 2 MS better but a bit cleaner
+    def isSubsequence_2(self, s: str, t: str) -> bool:
+        if not s:
+            return True
+
+        index = 0
+
+        for i in t:
+            if s[index] == i:
+                index += 1
+            if index == len(s):
+                return True
+        
         return False
